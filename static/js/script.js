3a11982b0bcf588ce1db84d5fd53142bd810bc92
ftrajano <ftrajano@gmail.com>
2019-12-14 09:35:44 -0300

    Baixeis as pastas do servidor e substitui as locais do git
    Baixei as pastas que estavam no servidor pq já tinha mexido muito nos arquivos locais estavam desatualizados.

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,28 @@
-var funcionario = {
-	nome:"Joao",
-	cargo:"Cozinheiro",
-	cor:"Negra",
-	sexo:"M",
-	gravidez: false,
-	displayFuncionario: function(){
-		console.log(this.nome);
+/* ---------------Variáveis--------------*/
+var listaFuncionarios = {
+	
+	funcionarios: [],
+
+	exibeLista: function(){
+		console.log('Funcionários cadastrados:')
+		for (var i=0; i < this.funcionarios.length; i++){
+			console.log(this.funcionarios[i].nome);
+		}
+	},
+
+	adicionaFuncionario: function(nome,cargo,cor,sexo){
+		
+		this.funcionarios.push({
+			nome:nome,
+			cargo:cargo,
+			cor:cor,
+			sexo:sexo,
+			gravidez:false
+		});
+		this.exibeLista()
 	}
-	 
-};
+}
+
 
 
 var displayFuncionariosButton = document.getElementById("displayFuncionariosButton");
@@ -20,8 +34,43 @@ displayFuncionariosButton.addEventListener('click', function(){
 var handlers = {
 	displayFuncionarios: function(){
 		funcionario.displayFuncionario()
+	},
+
+	addFuncionario: function(){
+		var addNomeInput = document.getElementById('addNomeInput')
+		var addCargoInput = document.getElementById('addCargoInput')
+		var addCorInput = document.getElementById('addCorInput')
+		var addSexoInput = document.getElementById('addSexoInput')
+
+		listaFuncionarios.adicionaFuncionario(addNomeInput.value, addCargoInput.value, addCorInput.value, addSexoInput.value);
+
+		addNomeInput.value='';
+		addCargoInput.value ='';
+		addCorInput.value ='';
+		addSexoInput.value ='';
+
+		view.displayFuncionarios();
+
 	}
 };
+
+var view = {
+	displayFuncionarios(){
+		var funcUl = document.querySelector('ul');
+		funcUl.innerHTML=''
+		for (var i=0; i<listaFuncionarios.funcionarios.length;i++){
+			var funcLi = document.createElement('li');
+			funcLi.textContent = listaFuncionarios.funcionarios[i].nome;
+			funcUl.appendChild(funcLi);
+		}
+	}
+};
+
+
+
+
+
+
 
 
 
